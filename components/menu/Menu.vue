fd14d42fc5dcf48b87a42f666160af28d5d7420a
Jason Franco Quintero <jason.francoquintero@plexus.es>
2020-09-08 09:19:15 +0200

    Add menu to sidebar

---
+++ components/menu/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu-content">
+    <h5>Productos</h5>
+    <ul>
+      <li>Portátiles</li>
+      <li>Ordenadores de Sobremesa</li>
+      <li>Móviles</li>
+
+      <li>Ofertas</li>
+    </ul>
+    <hr/>
+    <h5>Ofertas</h5>
+    <ul>
+        <li>Chollos de la semana</li>
+        <li>Ofertas con limite de stock</li>
+        <li>Ofertas con horas limitadas</li>
+    </ul>
+    <hr/>
+    <h5>Mi cuenta</h5>
+    <ul>
+        <li>Datos de mi cuenta</li>
+        <li>Mis favoritos</li>
+    </ul>
+  </div>
+</template>
+
+<style lang='sass'>
+  @import './Menu'
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,48 @@
 <template>
-    <div class="wrapper">
+  <div class="wrapper">
     <nav id="sidebar" class="active">
-        <nav class="navbar navbar-expand-lg navbar-light sidebar-header">
-        </nav>
-   
+      <nav class="navbar navbar-expand-lg navbar-light sidebar-header">
+        <span>
+          <img src="~assets/icons/usuario.svg" alt="icono de usuario" width="25px" height="25px" />
+          Hola Jasondfq
+        </span>
+      </nav>
+      <div class="menu-container">
+        <Menu />
+      </div>
     </nav>
     <div id="content">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <div>
-          <button type="button" class="btn"  v-on:click='sidebarCollapse'>
-            <img src="~assets/icons/parrilla.svg" width="25px" height="25px" alt="menu de hamburguesa" />
-          </button>
-           <NuxtLink to="/">
-           <button type="button" class="btn">
-              <img src="~assets/icons/logo_small.png" width="auto" height="30px" alt="logo de la tienda"/>
+            <button type="button" class="btn" v-on:click="sidebarCollapse">
+              <img
+                src="~assets/icons/parrilla.svg"
+                width="25px"
+                height="25px"
+                alt="menu de hamburguesa"
+              />
             </button>
-           </NuxtLink>
-           </div>
+            <NuxtLink to="/">
+              <button type="button" class="btn">
+                <img
+                  src="~assets/icons/logo_small.png"
+                  width="auto"
+                  height="30px"
+                  alt="logo de la tienda"
+                />
+              </button>
+            </NuxtLink>
+          </div>
           <NuxtLink to="/caddy">
             <button class="btn">
-              <img src="~assets/icons/carro-inteligente.png" width="25px" height="25px" alt="carrito de la compra"/>
-               <span>Mi carrito</span>
+              <img
+                src="~assets/icons/carro-inteligente.png"
+                width="25px"
+                height="25px"
+                alt="carrito de la compra"
+              />
+              <span>Mi carrito</span>
             </button>
           </NuxtLink>
         </div>
@@ -34,14 +55,15 @@
 </template>
 <script>
 export default {
-     name: 'app',
-     methods: {
-         sidebarCollapse() {
-             console.log('HOLA MUNDO');
-             document.getElementById('sidebar').className === 'active' ? 
-                document.getElementById('sidebar').className = '' : document.getElementById('sidebar').className = 'active'
-            //  $('#sidebar').toggleClass('active');
-         }
-     }
-}
+  name: "app",
+  methods: {
+    sidebarCollapse() {
+      console.log("HOLA MUNDO");
+      document.getElementById("sidebar").className === "active"
+        ? (document.getElementById("sidebar").className = "")
+        : (document.getElementById("sidebar").className = "active");
+      //  $('#sidebar').toggleClass('active');
+    }
+  }
+};
 </script>
