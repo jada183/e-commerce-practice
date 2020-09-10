17ed542f5ebc98d39529080493b5af422c29ad14
Jason Franco Quintero <jason.francoquintero@plexus.es>
2020-09-10 11:09:23 +0200

    Add containter class to all view contents

--- pages/caddy.vue
+++ pages/caddy.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <h3>Lista de la compra</h3>
     </div>
 </template>
--- pages/products.vue
+++ pages/products.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h5>Productos</h5>
   </div>
 </template>
