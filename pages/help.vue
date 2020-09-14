be1055faf21fa0ca398c3a5433fb47d8d2f8b3d8
Jason Franco Quintero <jason.francoquintero@plexus.es>
2020-09-14 13:50:29 +0200

    Add help view content

--- pages/help.vue
+++ pages/help.vue
@@ -1,5 +1,72 @@
 <template>
     <div class="container">
-        <h3>Ayuda</h3>
+        <div class="row">
+            <h5 class="col-12">Como realizar tus compras</h5>
+        </div>
+        <div class="row mt-3">
+            <ol class="offset-1">
+                <li>Iniciar sesión o crear cuenta.
+                    <ul>
+                        <li>Puedes acceder mediante el boton de Iniciar sesión de la cabecera en la parte superior derecha</li>
+                        <li>Puedes acceder mediante el boton de Hola identificate que se encuentra en la cabecera del menu lateral que se despliega con el boton que se encuentra
+                            en la parte superior izquierda de la cabecera.
+                        </li>
+                        <li>Para crear una cuenta tienes que acceder de alguna de las formas anteriormente explicadas a la ventana de Iniciar sesion y en la parte inferior del
+                            formulario de iniciar sesión tienes un boton de crear cuenta.
+                        </li>
+                    </ul>
+                </li>
+                <li>Añadir productos al carrito
+                    <ul>
+                        <li>Para agregar un producto al carrito hemos de pulsar la caja de cualquier producto independientemente de si se encuentra en la pagina de inicio,
+                            en la ventana de ofertas o de productos, esto te redirigira a la ventana del detalle del producto.
+                        </li>
+                        <li>
+                            En la ventana del detalle producto dispondras una caja de texto donde podras introducir la cantidad de un producto que deseas y ademas un boton azul 
+                            que te permitira añadir el producto al carrito.
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    Confirmar compra
+                    <ul>
+                        <li>Para confirmar compra debes ir a la ventana de carrito que accedes mediante el boton de carrito situado arriba a la derecha de la cabecera de la página</li>
+                        <li>En la ventana de carrito dispondras de un boton de confirmar compra ademas de la opción de eliminar algun producto del carrito o de limpiar todo el carrito</li>
+                        <li>Se podra añadir productos sin haber iniciado sesión pero no se podra confirmar la compra hasta que no se haya iniciado sesión.</li>
+                    </ul>
+                </li>
+                <li>
+                    Favoritos
+                    <ul>
+                        <li>disponemos de una opcion en el detalle del producto para añadir a favoritos , pulsando un boton con un corazon en el detalle de producto a la izquierda del boton
+                            de añadir al carrito.
+                        </li>
+                        <li>El corazón sera rojo si el producto ya se encuentra en la lista de favoritos y gris sino lo esta </li>
+                        <li>Para ver la lista de favoritos puedes acceder abriendo el menu lateral con el boton situado en la parte superior izquierda de la cabecera y
+                            pulsando en mis favoritos, Si no has iniciado sesion no podras ver dicho enlace en el menu lateral
+                        </li>
+                    </ul>
+                </li>
+                <li>Gestion de tu cuenta
+                    <ul>
+                        <li>Una forma de poder cambiar los datos de tu cuenta es mediante el boton mi cuenta que se encuentra en la parte derecha de la cabecera junto
+                            al boton de Mi carrito.este solo aparecera si no has iniciado sesión.
+                        </li>
+                        <li>
+                            Otra forma es accediendo desde el boton de la cabecera del menu lateral, en el que aparecera un 'hola' y seguido tu nombre siempre que hayas
+                            iniciado sesión.
+                        </li>
+                        <li>
+                            la tercera forma es la opcion del menu lateral Datos de mi cuenta que aparecera si has iniciado sesión.
+                        </li>
+                    </ul>
+                </li>
+            </ol>
+        </div>
     </div>
 </template>
+<style scoped>
+    li{
+        margin-top: 2%;
+    }
+</style>
