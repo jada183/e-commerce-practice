a0f701b3f7e9bc912dc98fa72a9dbb8d25b4613c
Jason <jada183.jf@gmail.com>
2020-09-17 10:04:48 +0200

    Add styles to show response favorite view

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -7,47 +7,48 @@
       <div class="col-12">
         <div class="table-container">
           <div class="row table-header bg-light">
-            <span class="col-5">ARTÍCULO</span>
-            <span class="col-2">PRECIO</span>
-            <span class="col-2">AÑADIR AL CARRITO</span>
-            <span class="col-2 last-column">TOTAL</span>
+            <span class="col-3 col-lg-5">ARTÍCULO</span>
+            <span class="col-3 col-lg-2">PRECIO</span>
+            <span class="col-3 col-lg-2">AÑADIR <span class="no-show-mobile"> AL CARRITO</span></span>
+            <span class="col-3 col-lg-2 last-column">ELIMINAR</span>
           </div>
           <div class="row mb-3 content-row" v-for="item in itemList">
-            <div class="col-5 item-title-img-link">
+            <div class="col-3 col-lg-5 item-title-img-link product-name">
               <NuxtLink
                 v-bind:to="'/product-detail?id=' + item.id"
                 class="item-title-img-container"
               >
                 <img
                   v-bind:src="item.image"
-                  class="col-4 caddy-item-image"
+                  class="col-lg-4 col-12 caddy-item-image"
                   height="100%"
                   alt="Imagen del producto"
                 />
-                <div class="col-7">
-                  <span>{{ item.name }}</span>
+                <div class="col-lg-7 col-0">
+                  <p class="no-show-mobile">{{ item.name }}</p>
                 </div>
               </NuxtLink>
             </div>
 
-            <span class="col-2">{{item.price}}€</span>
-            <span class="col-2">
+            <span class="col-3 col-lg-2">{{item.price}}€</span>
+            <span class="col-3 col-lg-2">
               <button class="btn btn-primary">
                 <img
                   src="~assets/icons/carro-inteligente.png"
                   width="25px"
                   height="25px"
                   alt="carrito de la compra"
+                  class="caddy-button"
                 />
-                <span class="pl-3">Añadir</span>
+                <span class="pl-3 no-show-mobile">Añadir</span>
               </button>
             </span>
-            <span class="col-2 last-column">{{ item.price *item.quantity}}€</span>
-            <span class="col-1 remove-item">x</span>
+            <span class="col-3 last-column col-lg-2 remove-item">X</span>
+
           </div>
         </div>
         <div class="row mb-5">
-          <div class="col-lg-4 col-10 offset-1 offset-lg-0">
+          <div class="col-lg-4 col-10">
             <button class="btn btn-light">
               <img
                 src="~assets/icons/basura.svg"
@@ -55,7 +56,7 @@
                 height="25px"
                 alt="Contenedor de basura"
               />
-              <span class="pl-3">Limpiar favoritos</span>
+              <span class="pl-3 font-size-fix">Limpiar favoritos</span>
             </button>
           </div>
         </div>
@@ -124,5 +125,13 @@ export default {
 }
 .remove-item:hover {
   text-decoration: underline;
+}
+@media screen and (max-width: 992px) {
+  span {
+    font-size: 2em;
+  }
+  .product-name {
+    overflow: hidden;
+  }
 }
 </style>
